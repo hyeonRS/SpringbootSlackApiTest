6065a926f838e5dd7f9cb36012f43d1002196ed3
hhsk908 <hhsk0908@naver.com>
2026-01-05 13:41:00 +0900

    first Commit

---
+++ src/main/java/com/mycom/myapp/controller/SlackApiController.java
@@ -0,0 +1,24 @@
+package com.mycom.myapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.mycom.myapp.service.SlackApiService;
+
+@Controller
+public class SlackApiController {
+
+	private final SlackApiService slackApiService;
+
+	public SlackApiController(SlackApiService slackApiService) {
+		this.slackApiService = slackApiService;
+	}
+	
+	@GetMapping("/notify")
+	public String sendSlackNotification() {
+		slackApiService.sendMessage("🔔 SpringbootSlackApiTest App 메세지를 전송했습니다.");
+		return "Slack에 Message를 보냈습니다.";
+	}
+	
+	
+}
